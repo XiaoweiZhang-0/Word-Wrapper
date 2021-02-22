94e27bd36384bb19c80d12a86a7c001c36c07752
Xiaowei Zhang <blackcatecho1998@gmail.com>
2021-02-22 03:14:10 -0500

    finish assignment 2

--- Assignment/Assignment2/arraylist.c
+++ Assignment/Assignment2/arraylist.c
@@ -51,5 +51,52 @@ int al_remove(arraylist_t *L, int *item)
 
     if (item) *item = L->data[L->used];
 
-    return 1;
+    return 0;
+}
+
+int al_insert(arraylist_t *list, int index, int item)
+{
+    if(index<0)
+    {
+        return 1;
+    }
+    if(list->length<=index)
+    {
+        int new_length = (list->length)*2;
+        if ((list->length)*2<index+1)
+        {
+            new_length=index+1;
+        }
+        list->length=new_length;
+        list->data=realloc(list->data, sizeof(int)*new_length);
+        if(!list->data)
+        {
+            return 1;
+        }
+        list->used=index+1;
+    }
+    else
+    {
+        if(list->used<=index)
+        {
+            //list->data=realloc(list->data, sizeof(int)*(index+1));
+            list->used = index+1;
+        }
+        else
+        {
+            if(list->length==list->used)
+            {
+                list->length=list->length*2;
+                list->data=realloc(list->data, sizeof(int)*list->length);
+            }
+            list->used++;
+            for(int i=list->used-1; i>index; i--)
+            {
+                list->data[i]=list->data[i-1];
+            }
+        }
+    }
+
+    list->data[index]=item;
+    return 0;
 }
--- Assignment/Assignment2/arraylist.h
+++ Assignment/Assignment2/arraylist.h
@@ -8,3 +8,4 @@ int al_init(arraylist_t *, size_t);
 void al_destroy(arraylist_t *);
 int al_append(arraylist_t *, int);
 int al_remove(arraylist_t *, int *);
+int al_insert(arraylist_t *, int, int);
